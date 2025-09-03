aa2afffbcdfe3ee181adc2ddc1b40d8829e07ce5
Egor <wehw93@mail.ru>
2025-09-03 19:01:34 +0300

    init commit

---
+++ src/test/java/school/sorokin/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sorokin.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
